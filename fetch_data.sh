1d00b9ea1ed2f6c3d4cefffaf7d716a867c92a65
Stephan Kurz <stephan.kurz@oeaw.ac.at>
2022-12-13 14:39:57 +0100

    replace wget by curl -L0

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,5 +1,5 @@
 rm -rf data
-wget https://github.com/nuntiaturberichte/nbr-pius-xi-data/archive/refs/heads/default.zip
+curl -LO https://github.com/nuntiaturberichte/nbr-pius-xi-data/archive/refs/heads/default.zip
 unzip default.zip
 mv nbr-pius-xi-data-default/TEI ./data
 rm -rf nbr-pius-xi-data-default
